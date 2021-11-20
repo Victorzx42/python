e86703d7079f95f975a5ef669ea5bf1bd6308d75
victor morales <victorzx42@gmail.com>
2021-11-19 22:15:43 -0500

    cambio a buenas praticas ZZ ZZ ZXCjkm.

--- conversor.py
+++ conversor.py
@@ -7,8 +7,9 @@ def conversor(tipo_pesos, valor_dolar):
     dolares = str(dolares)
     print("tienes $" + dolares + " pesos " + tipo_pesos)
 
+if __name__ == "__main__":    
 
-menu = """"
+    menu = """"
 1 colombia
 2 argentina
 3 mexicamos
@@ -25,3 +26,12 @@ elif opcion == 3:
     conversor("mexicanos", 24)
 else:
     print("ingresa un numero correcto porfa ;(")
+    
+    
+
+
+    
+
+
+
+
